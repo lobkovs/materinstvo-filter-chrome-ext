bf8ef077709f486d85afc183af8e9cf927344156
Лобков Александр <aslobkov@mts.ru>
2019-01-27 02:37:26 +0300

    Разработан 'умный' диапазон

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,7 +3,8 @@
   data () {
     return {
       inputArray: "",
-      separators: [',', ';', '.']
+      separators: [',', ';', '.'],
+      rangeSeparator: '-',
     }
   },
   created () {
@@ -21,22 +22,56 @@
   watch: {
     // Защита от ввода букв и спец. символов
     inputArray: function (newWord) {
-      this.inputArray = newWord.replace(/[^\d,;\.\s]/g, '')
+      this.inputArray = newWord.replace(/[^\d,;\.\-\s]/g, '')
     }
   },
   computed: {
     // Возвращает актуальный массив из строки
     indexes () {
-      let split = this.inputArray ? this.inputArray.split(this.getRegExp) : []
+      let split = this.inputArray.split(this.getRegExp) || []
       // Убираем пустые и возвращаем массив чисел
-      return split.map(x => parseInt(x))
+      return split.reduce((acc, item) => {
+        // item = item.toString().trim()
+        if (item.includes(this.rangeSeparator)) {
+          return acc.concat(this.getRangeArray(item))
+        } else {
+          return acc.concat(+item)
+        }
+      }, [])
     },
     getRegExp () {
       return new RegExp(`[${this.separators.join('|')}]`, 'g')
     }
   },
   methods: {
-    filter () {
+    getRangeArray (item) {
+      const split = item.split(this.rangeSeparator)
+      // Init vars
+      let rangeArray = []
+      let startItem = 0
+
+      const last = +split.pop()
+      const first = +split.shift()
+
+      let diffCount
+
+      // Revert if first more last and init start value
+      if (first > last) {
+        diffCount = first - last
+        startItem = last
+      } else {
+        diffCount = last - first
+        startItem = first
+      }
+
+      // Create range array
+      for (let i = 0; i <= diffCount; i++) {
+        rangeArray.push(startItem + i)
+      }
+
+      return rangeArray
+    },
+    doFilter () {
       // Положим в chrome хранилице введённые пользователем номера
       chrome.storage.sync.set({ items: this.indexes }, function() {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
@@ -50,7 +85,7 @@
     },
     clear () {
       this.inputArray = ""
-      this.filter()
+      this.doFilter()
     }
   }
 })
